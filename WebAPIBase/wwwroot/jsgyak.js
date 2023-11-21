b6868dbeeba7e2601a36ff070b16684ea4c9444f
BalogTerez <terez.balog@stud.uni-corvinus.hu>
2023-11-21 11:06:03 +0100

    uj

--- WebAPIBase/wwwroot/jsgyak.js
+++ WebAPIBase/wwwroot/jsgyak.js
@@ -1,1 +1,28 @@
-﻿
+﻿window.onload = function () { 
+    for (var sor = 0; sor < 10; sor++) {
+        var ujdiv = document.createElement('div');
+        //új div létrehozása az új sornak
+
+        ujdiv.classList.add('sor');
+        //új div osztálylistájához add hozzá a "sor"-t
+
+        var pascaldiv = document.getElementById('pascal');
+        pascaldiv.appendChild(ujdiv);
+        //új div-et add hozzá a "pascal" gyermekeihez
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            //új div létrehozása az új elemnek
+            var ujelemdiv = document.createElement('div');
+
+            //új elem div osztálylistájához add hozzá az "elem"-et
+            ujelemdiv.classList.add('elem');
+
+            //teszteléshet .innerHTML = `${sor}:${oszlop}`
+            ujelemdiv.innerText = `${sor}:${oszlop}`
+            //legyen az innerHTML a megfelelő szám
+
+            ujdiv.appendChild(ujelemdiv);
+            //új elem div-et vedd fel a sor elemei közé
+        }
+    }
+};
